b8ff18ff3faa6b4512d16c54b281437df1964c8a
ViLevin <vilevinqa@gmail.com>
2025-06-08 23:30:09 +0300

    tmp

--- src/main/java/book/odata/config/ODataServiceFactory.java
+++ src/main/java/book/odata/config/ODataServiceFactory.java
@@ -3,26 +3,34 @@ package book.odata.config;
 import javax.persistence.EntityManagerFactory;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAContext;
 import org.apache.olingo.odata2.jpa.processor.api.ODataJPAServiceFactory;
+import org.apache.olingo.odata2.jpa.processor.api.exception.ODataJPARuntimeException;
 import org.apache.olingo.odata2.jpa.processor.core.ODataJPAContextImpl;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class ODataServiceFactory extends ODataJPAServiceFactory {
 
     private static final String PERSISTENCE_UNIT_NAME = "default";
-    private static EntityManagerFactory emf;
+    private static volatile EntityManagerFactory emf;
 
-    public static void setEmf(EntityManagerFactory emfInput) {
-        emf = emfInput;
+    public static synchronized void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        System.out.println("Setting EMF in ODataServiceFactory: " + (entityManagerFactory != null));
+        ODataServiceFactory.emf = entityManagerFactory;
     }
 
     @Override
-    public ODataJPAContext initializeODataJPAContext() {
-        ODataJPAContextImpl context = new ODataJPAContextImpl();
-        context.setEntityManagerFactory(emf);
-        context.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
-        return context;
+    public ODataJPAContext initializeODataJPAContext() throws ODataJPARuntimeException {
+        System.out.println("ODataServiceFactory.initializeODataJPAContext() called");
+        System.out.println("EMF is null: " + (emf == null));
+        System.out.println("This object: " + this.getClass().getName() + "@" + Integer.toHexString(this.hashCode()));
+        
+        if (emf == null) {
+            throw new RuntimeException("EntityManagerFactory is null in ODataServiceFactory");
+        }
+        
+        ODataJPAContextImpl oDataJPAContext = new ODataJPAContextImpl();
+        oDataJPAContext.setEntityManagerFactory(emf);
+        oDataJPAContext.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
+        return oDataJPAContext;
     }
 
 }
--- src/main/java/book/odata/config/ODataServletConfig.java
+++ src/main/java/book/odata/config/ODataServletConfig.java
@@ -1,6 +1,9 @@
 package book.odata.config;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.apache.olingo.odata2.core.servlet.ODataServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +11,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ODataServletConfig {
 
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
     @Bean
     public ServletRegistrationBean<ODataServlet> odataServlet() {
+        System.out.println("Creating OData servlet, EMF is null: " + (entityManagerFactory == null));        
+        
+        ODataServiceFactory.setEntityManagerFactory(entityManagerFactory);
+        
         ServletRegistrationBean<ODataServlet> srb = new ServletRegistrationBean<>(new ODataServlet(), "/odata.svc/*");
-        srb.setLoadOnStartup(1);         
-
+        srb.setLoadOnStartup(1);
+        
+        srb.addInitParameter("org.apache.olingo.odata2.service.factory", 
+                            "book.odata.config.ODataServiceFactory");
+        
         return srb;
     }
 }
